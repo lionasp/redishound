a495d049f7a9d2cf7a21a73e362c00708735fc05
lionasp <lion.programmer@gmail.com>
2020-11-14 17:47:21 +0300

    add Key class

---
+++ redishound/src/main/java/com/lionasp/connector/key/Key.java
@@ -0,0 +1,22 @@
+package com.lionasp.connector.key;
+
+public class Key {
+    final private String name;
+    private String type;
+
+    public Key(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
